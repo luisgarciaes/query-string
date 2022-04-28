f15a06712f8aaf8b1438f149f73b3144e2137278
ruruisu <zaddozgl@gmail.com>
2022-04-27 21:04:19 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('view engine', 'ejs')
+app.use('/', (req, res, next) => {
+    console.log('Request URL:'+ req.url);
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.send(`<html><head><link rel='stylesheet' href='/assets/style.css'>
+  <title>Document </title></head>
+  <body><h1>Hola mundo</h1>
+  <p> Este es un parrafo y su contenido debe ser azul</p></body></html>`)
+}
+);
+
+app.get('/person/:id', (req, res) => {
+    res.send(`<!DOCTYPE html><html lang='en'><link rel='stylesheet' href='assets/style.css'></html>`)
+});
+
+app.listen(PORT);
+
+
